60ad8f37e62e2c9395c74ff837e78aa109d700f5
Amalie Forum <amalieforum@live.dk>
2023-02-06 11:56:26 +0100

    discount

--- produktliste.js
+++ produktliste.js
@@ -14,8 +14,11 @@ function visData(json) {
   json.forEach((produkt) => {
     const klon = skabelon.cloneNode(true);
     klon.querySelector(".overskriftpl3").textContent = produkt.productdisplayname;
-    klon.querySelector(".pris3").textContent = produkt.price;
+    klon.querySelector(".pris").textContent = produkt.price;
     klon.querySelector(".tekstpl3").textContent = produkt.articletype;
+    klon.querySelector(".brand").textContent = produkt.brandname;
+    klon.querySelector(".procent").textContent = produkt.discount;
+
     if (produkt.soldout) {
       klon.querySelector("article").classList.add("soldOut");
     }
@@ -28,39 +31,3 @@ function visData(json) {
 }
 
 hentData(filNavn);
-
-// async function getData() {
-//   const response = await fetch("https://kea-alt-del.dk/t7/api/products");
-//   const data = await response.json();
-//   data.forEach(showProduct);
-// }
-
-// getData();
-
-// function showProduct(product) {
-//   console.log(product);
-//   const template = document.querySelector("#smallProductTemplate").content;
-//   const copy = template.cloneNode(true);
-//   copy.querySelector(".overskriftpl3").textContent = product.productdisplayname;
-//   if (product.soldout) {
-//     copy.querySelector("article").classList.add("soldOut");
-//   }
-//   if (product.discount) {
-//     copy.querySelector("article").classList.add("onSale");
-//   }
-//   document.querySelector("main").appendChild(copy);
-// }
-
-// function showProduct(product) {
-//     console.log(product);
-//     const template = document.querySelector("#smallProductTemplate").content;
-//     const copy = template.cloneNode(true);
-//     copy.querySelector("h3").textContent = product.productdisplayname;
-//     if (product.soldout) {
-//       copy.querySelector("article").classList.add("soldOut");
-//     }
-//     if (product.discount) {
-//       copy.querySelector("article").classList.add("onSale");
-//     }
-//     document.querySelector("main").appendChild(copy);
-//   }

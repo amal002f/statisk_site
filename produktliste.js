201872c62fe494534a6518fbfef2519aeb8d2cd2
Amalie Forum <amalieforum@live.dk>
2023-02-06 08:45:01 +0100

    udsolgt produktliste

--- produktliste.js
+++ produktliste.js
@@ -20,8 +20,11 @@ function visData(json) {
     klon.querySelector(".overskriftpl3").textContent = produktliste.productdisplayname;
     klon.querySelector(".pris3").textContent = produktliste.price;
     klon.querySelector(".tekstpl3").textContent = produktliste.articletype;
-    if (json.soldout) {
-      klon.querySelector("article").classList.add("produktUdsolgt");
+    if (produktliste.soldout) {
+      klon.querySelector("article").classList.add("soldOut");
+    }
+    if (produktliste.discount) {
+      klon.querySelector("article").classList.add("onSale");
     }
     // klon.querySelector(".produktimg3").src = imagePath;
     beholder.appendChild(klon);
@@ -29,3 +32,39 @@ function visData(json) {
 }
 
 hentData(filNavn);
+
+// async function getData() {
+//   const response = await fetch("https://kea-alt-del.dk/t7/api/products");
+//   const data = await response.json();
+//   data.forEach(showProduct);
+// }
+
+// getData();
+
+// function showProduct(product) {
+//   console.log(product);
+//   const template = document.querySelector("#smallProductTemplate").content;
+//   const copy = template.cloneNode(true);
+//   copy.querySelector(".overskriftpl3").textContent = product.productdisplayname;
+//   if (product.soldout) {
+//     copy.querySelector("article").classList.add("soldOut");
+//   }
+//   if (product.discount) {
+//     copy.querySelector("article").classList.add("onSale");
+//   }
+//   document.querySelector("main").appendChild(copy);
+// }
+
+// function showProduct(product) {
+//     console.log(product);
+//     const template = document.querySelector("#smallProductTemplate").content;
+//     const copy = template.cloneNode(true);
+//     copy.querySelector("h3").textContent = product.productdisplayname;
+//     if (product.soldout) {
+//       copy.querySelector("article").classList.add("soldOut");
+//     }
+//     if (product.discount) {
+//       copy.querySelector("article").classList.add("onSale");
+//     }
+//     document.querySelector("main").appendChild(copy);
+//   }
